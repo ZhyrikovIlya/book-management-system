b45060fffff71aaef5a89b533e963bf9e30beb25
Ilya_Zhyrikov <zhyrikov.ilya@lll.kpi.ua>
2024-12-27 23:59:35 +0200

    Виявлено баг та виправлено код в index.js, зробив так щоб нічого нового книгам не присвоювалося, а виводилося на основі даних з бази даних, в частині Marked as read: виводилися тільки прочитані книги без присвоєння книгам true, в частині Added review: не додавалися додаткові відгуки, переписано опис фільтраціїnode src/index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { getBooks, markAsRead, addReview } = require('./controllers/bookController');
+const { getBooks } = require('./controllers/bookController');
 
 async function main() {
     console.log('Book Management System');
@@ -7,27 +7,21 @@ async function main() {
     const books = await getBooks();
     console.log('Books loaded from database:', books);
 
-    // Позначка книги як прочитаної
-    const bookToMark = books.find((book) => book.title === 'The Hobbit');
-    if (bookToMark) {
-        const updatedBook = await markAsRead(bookToMark.id);
-        console.log('Marked as read:', updatedBook);
-    }
+    // Висновок лише прочитаних книг
+    const readBooks = books.filter((book) => book.isRead);
+    console.log('Marked as read:', readBooks);
 
-    // Додавання відгуку
-    const bookToReview = books.find((book) => book.title === '1984');
-    if (bookToReview) {
-        const reviewedBook = await addReview(bookToReview.id, 'A chilling dystopian novel.');
-        console.log('Added review:', reviewedBook);
-    }
+    // Виведення книг з відгуками
+    const booksWithReviews = books.filter((book) => book.reviews && book.reviews.length > 0);
+    console.log('Books with reviews:', booksWithReviews);
 
     // Фільтрування за жанром
     const fantasyBooks = books.filter((book) => book.genre === 'Fantasy');
-    console.log('Fantasy books:', fantasyBooks);
+    console.log('Books by genre:', fantasyBooks);
 
     // Фільтрування за автором
     const tolkienBooks = books.filter((book) => book.author === 'J.R.R. Tolkien');
-    console.log('Books by J.R.R. Tolkien:', tolkienBooks);
+    console.log('Books by specified author:', tolkienBooks);
 
     // Пошук за назвою
     const searchTitle = '1984';
